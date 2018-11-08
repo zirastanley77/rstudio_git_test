cfc9532b01ca35c228ae574bf070f89e1b417ed1
Stanley Zira <zirastanley77@gmail.com>
2018-11-08 11:38:09 +0100

    first script

---
+++ Scripts/plot_surveys.R
@@ -0,0 +1,15 @@
+# Install the tidyverse if not already installed
+if (!requireNamespace("tidyverse"))
+  install.packages("tidyverse")
+# loading library
+library("tidyverse")
+# loading datafile
+surveys_complete <- read_csv("data/surveys_complete.csv")
+
+# Assign plot to a variable
+surveys_plot <- ggplot(data = surveys_complete,
+                       mapping = aes(x = weight, y = hindfoot_length))
+
+# Draw the plot
+surveys_plot +
+  geom_point()
